2711ba13af414e90ca2870281bb4d00967060a4d
biancapcr <biancacristina.pocora@gmail.com>
2025-07-09 15:45:21 +0200

    [add] listener

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ const bulbImg = document.getElementById('bulb');
 
 // Seleziono button tramite il suo id
 const toggleBtn = document.getElementById('toggle-btn');
+
+// Aggiungo un listener sul click del bottone
+toggleBtn.addEventListener('click', () =>{
+    // Controllo se il testo del bottone contiene "Accendi"
+    if (toggleBtn.textContent.includes('Accendi')){
+        // Se sì, "accesa"
+        bulbImg.src = 'img/yellow_lamp.png';
+        // Cambio il testo del bottone in "Spegni"
+        toggleBtn.textContent = 'Spegni';
+    }
+});
