34947dbd2b1e09970233b50e4bed744d02dece9c
biancapcr <biancacristina.pocora@gmail.com>
2025-07-09 17:00:27 +0200

    [fix] script.js structure

--- js/script.js
+++ js/script.js
@@ -5,21 +5,22 @@ const bulbImg = document.getElementById('bulb');
 const toggleBtn = document.getElementById('toggle-btn');
 
 // Aggiungo un listener sul click del bottone
-toggleBtn.addEventListener('click', () =>{
-    let isOn;
-    // Controllo se il testo del bottone contiene "Accendi"
-  if (toggleBtn.textContent.includes('Accendi')){
-        // Se sì, "accesa"
-        bulbImg.src = 'img/yellow_lamp.png';
-        toggleBtn.textContent = 'Spegni';
-        isOn = true;
-    }
-  else{
-        // Altrimenti ("Spegni"), ripristino la lampadina "spenta"
-        bulbImg.src = 'img/white_lamp.png';
-        toggleBtn.textContent = 'Accendi';
-        isOn = false;
-    }
-    // Stampa
-    console.log(`Lampadina ${isOn ? 'accesa' : 'spenta'}`);
+toggleBtn.addEventListener('click', () => {
+  let isOn;
+  // Controllo se il testo del bottone contiene "Accendi"
+  if (toggleBtn.textContent.includes('Accendi')) {
+    // Se sì, "accesa"
+    bulbImg.src = 'img/yellow_lamp.png';
+    toggleBtn.textContent = 'Spegni';
+    isOn = true;
+  }
+  else {
+    // Altrimenti ("Spegni"), ripristino la lampadina "spenta"
+    bulbImg.src = 'img/white_lamp.png';
+    toggleBtn.textContent = 'Accendi';
+    isOn = false;
+  }
+
+  // Stampa
+  console.log(`Lampadina ${isOn ? 'accesa' : 'spenta'}`);
 });
