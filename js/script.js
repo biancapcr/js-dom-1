2129d245b5dbb1cffef2b22c90d529ac9ca824f1
biancapcr <biancacristina.pocora@gmail.com>
2025-07-09 15:25:38 +0200

    [add] use const to select dom elements

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Seleziono img della lampadina tramite il suo id
+const bulbImg = document.getElementById('bulb');
+
+// Seleziono button tramite il suo id
+const toggleBtn = document.getElementById('toggle-btn');
